5ee1c745264d0d4fe0a90d13c64f27a1ed143828
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-02-26 19:19:28 -0300

    Create composable auth supabase

---
+++ src/composables/UseAuthUser.js
@@ -0,0 +1,55 @@
+import { ref } from 'vue'
+import useSupabase from 'boot/supabase'
+
+const user = ref(null)
+
+export default function useAuthUser () {
+  const { supabase } = useSupabase()
+
+  const login = async ({ email, password }) => {
+    const { user, error } = await supabase.auth.signInWithPassword({ email, password })
+    if (error) throw error
+    return user
+  }
+
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (error) throw error
+  }
+
+  const isLoggedIn = () => {
+    return !!user.value
+  }
+
+  const register = async ({ email, password, ...meta }) => {
+    const { user, error } = await supabase.auth.signUp({ email, password }, {
+        data: meta,
+        redirectTo: `${window.location.origin}/me?fromEmail=registrationConfirmation`
+    })
+    if (error) throw error
+    return user
+  }
+
+  const update = async (data) => {
+    const { user, error } = await supabase.auth.update(data)
+    if (error) throw error
+    return user
+  }
+
+  const sendPasswordRestEmail = async () => {
+    const { user, error } = await supabase.auth.api.resetPasswordForEmail(data)
+    if (error) throw error
+    return user
+  }
+
+  return {
+    user,
+    login,
+    loginWithSocialProvider,
+    logout,
+    isLoggedIn,
+    register,
+    update,
+    sendPasswordRestEmail
+  }
+}
