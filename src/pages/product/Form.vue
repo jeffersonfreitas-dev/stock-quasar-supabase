3b429db408ec137795c75114313a1aa33135dd07
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-03-05 17:40:55 -0300

    Create and list produts

---
+++ src/pages/product/Form.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-page padding>
+    <div class="row justify-center">
+      <div class="col-12 text-center">
+        <p class="text-h6">
+          Form Product
+        </p>
+      </div>
+      <q-form class="col-md-7 col-xs-12 col-sm-12 q-gutter-y-md" @submit.prevent="handleSubmit">
+        <q-input label="Name" v-model="form.name" :rules="[val => (val && val.length > 0) || 'Name is required']"/>
+        <q-editor label="Description" min-height="5rem" v-model="form.description"/>
+        <q-input label="Amount" type="number" v-model="form.amount" :rules="[val => (val && val.length > 0) || 'Amount is required']"/>
+        <q-input label="Price" v-model="form.price" :rules="[val => (val && val.length > 0) || 'Price is required']" prefix="R$"/>
+        <q-select v-model="form.category_id" :options="optionsCategory" label="Category" option-value="id" option-label="name" map-options emit-value />
+        <q-btn :label="isUpdate ? 'Update' : 'Save'" color="primary" class="full-width" rounded type="submit"/>
+        <q-btn label="Cancel" color="primary" class="full-width" rounded flat :to="{ name: 'product' }"/>
+      </q-form>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref, onMounted, computed } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import useApi from 'src/composables/UseApi'
+import useNotify from 'src/composables/UseNotify'
+
+export default defineComponent({
+  name: 'PageFormProduct',
+  setup () {
+    const table = 'product'
+    const router = useRouter()
+    const route = useRoute()
+    const { create, getById, update, list } = useApi()
+    const { notifyError, notifySuccess } = useNotify()
+    let product = {}
+    const optionsCategory = ref([])
+    const form = ref({
+      name: '',
+      description: '',
+      amount: 0,
+      price: 0,
+      category_id: ''
+    })
+
+    const isUpdate = computed(() => route.params.id)
+    onMounted(() => {
+      handleListCategory()
+      if (isUpdate.value) {
+        handleGetProduct(isUpdate.value)
+      }
+    })
+
+    const handleSubmit = async () => {
+      try {
+        if (isUpdate.value) {
+          await update(table, { ...form.value })
+          notifySuccess(`${form.value.name} updated successfully`)
+        } else {
+          await create(table, form.value)
+          notifySuccess(`${form.value.name} saved successfully`)
+        }
+        router.push({ name: 'product' })
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    const handleListCategory = async () => {
+      optionsCategory.value = await list('category')
+    }
+
+    const handleGetProduct = async (id) => {
+      try {
+        product = await getById(table, id)
+        form.value = product
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    return {
+      form,
+      handleSubmit,
+      isUpdate,
+      optionsCategory
+    }
+  }
+})
+</script>
--- src/pages/product/List.vue
+++ src/pages/product/List.vue
@@ -1,11 +1,19 @@
 <template>
   <q-page padding>
     <div class="row">
-      <q-table :rows="categories" :columns="columnsCategory" row-key="id" class="col-12" :loading="loading">
+      <q-table :rows="products" :columns="columnsProduct" row-key="id" class="col-12" :loading="loading">
         <template v-slot:top>
-          <span class="text-h6">Category</span>
+          <span class="text-h6">Product</span>
           <q-space />
-          <q-btn label="Add New" color="primary" icon="mdi-plus" dense :to="{ name: 'form-category' }" v-if="$q.platform.is.desktop"/>
+          <q-btn label="Add New" color="primary" icon="mdi-plus" dense :to="{ name: 'form-product' }" v-if="$q.platform.is.desktop"/>
+        </template>
+        <template v-slot:body-cell-img_url="props">
+          <q-td :props="props">
+            <q-avatar v-if="props.row.img_url">
+              <img :src="props.row.img_url" />
+            </q-avatar>
+            <q-avatar v-else color="primary" text-color="white" icon="mdi-image-off" />
+          </q-td>
         </template>
         <template v-slot:body-cell-actions="props">
           <q-td :props="props" class="q-gutter-x-sm">
@@ -24,7 +32,7 @@
       </q-table>
     </div>
     <q-page-sticky position="bottom-right" :offset="[18, 18]" v-if="$q.platform.is.mobile">
-      <q-btn fab icon="mdi-plus" color="primary" :to="{ name: 'form-category' }"/>
+      <q-btn fab icon="mdi-plus" color="primary" :to="{ name: 'form-product' }"/>
     </q-page-sticky>
   </q-page>
 </template>
@@ -35,57 +43,57 @@ import { useRouter } from 'vue-router'
 import useApi from 'src/composables/UseApi'
 import useNotify from 'src/composables/UseNotify'
 import { useQuasar } from 'quasar'
-import { columnsCategory } from './table'
+import { columnsProduct } from './table'
 
 export default defineComponent({
-  name: 'PageCategoryList',
+  name: 'PageProductList',
   setup () {
     const { list, remove } = useApi()
     const { notifyError, notifySuccess } = useNotify()
     const router = useRouter()
-    const categories = ref([])
+    const products = ref([])
     const loading = ref(true)
-    const table = 'category'
+    const table = 'product'
     const $q = useQuasar()
 
-    const handleListCategories = async () => {
+    const handleListProducts = async () => {
       try {
         loading.value = true
-        categories.value = await list(table)
+        products.value = await list(table)
         loading.value = false
       } catch (error) {
         notifyError(error.message)
       }
     }
 
-    const handleRemove = async (category) => {
+    const handleRemove = async (product) => {
       try {
         $q.dialog({
           title: 'Confirm',
-          message: `Do you really delete ${category.name} ?`,
+          message: `Do you really delete ${product.name} ?`,
           cancel: true,
           persistent: true
         }).onOk(async () => {
-          await remove(table, category.id)
-          notifySuccess(`${category.name} removed successfully`)
-          handleListCategories()
+          await remove(table, product.id)
+          notifySuccess(`${product.name} removed successfully`)
+          handleListProducts()
         })
       } catch (error) {
         notifyError(error.message)
       }
     }
 
-    const handleEdit = (category) => {
-      router.push({ name: 'form-category', params: { id: category.id } })
+    const handleEdit = (product) => {
+      router.push({ name: 'form-product', params: { id: product.id } })
     }
 
     onMounted(() => {
-      handleListCategories()
+      handleListProducts()
     })
 
     return {
-      columnsCategory,
-      categories,
+      columnsProduct,
+      products,
       loading,
       handleEdit,
       handleRemove
