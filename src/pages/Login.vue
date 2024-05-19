7a419278e1dbd205f0f5970ff9d2e29000c6ab9a
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2024-05-19 16:48:43 -0300

    Tela de loning

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,45 +1,52 @@
 <template>
   <q-page padding>
-    <div class="row justify-center">
-      <img src="../../public/icons/apple-icon-120x120.png" class="q-mt-lg" alt="logo"/>
+    <div class="row justify-center q-mb-lg">
+      <img src="../../public/icons/apple-icon-120x120.png" class="q-mt-lg q-mb-lg" alt="logo"/>
     </div>
-    <q-form class="row justify-center" @submit.prevent="handleLogin">
+    <q-form class="row justify-center align-items-center" @submit.prevent="handleLogin">
       <div class="col-md-4 col-sn-6 col-xs-10 q-gutter-y-md">
-        <q-input
-          :label="$t('entity_email')"
-          v-model="form.email"
-          :rules="[val => (val && val.length > 0) || $t('required')]"
-          type="email"
-        />
-        <q-input
-          type="password"
-          :label="$t('entity_password')"
-          v-model="form.password"
-          :rules="[val => (val && val.length > 0) || $t('required')]"
-        />
-        <div class="full-width q-pt-md">
-          <q-btn
-            :label="$t('btn_login')"
-            color="primary"
-            class="full-width"
-            rounded
-            outline
-            type="submit"
-          />
+
+        <q-input v-model="form.email" filled type="email" :rules="[val => (val && val.length > 0) || $t('required')]" hint="E-mail">
+          <template v-slot:append>
+            <q-icon name="mail" />
+          </template>
+        </q-input>
+
+        <q-input v-model="form.password" filled :type="isPwd ? 'password' : 'text'" hint="Senha" :rules="[val => (val && val.length > 0) || $t('required')]">
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+
+        <div class=" row justify-center">
+          <q-btn align="around"
+          class="btn-fixed-width q-py-md"
+          icon="login"
+          color="primary"
+          :label="$t('btn_login')"
+          type="submit"/>
         </div>
-        <div class="full-width q-gutter-y-sm">
+
+        <div class=" row justify-center q-mt-lg">
           <q-btn
-            :label="$t('btn_register')"
-            color="primary"
-            class="full-width"
-            size="sm"
-            flat to="/register"
-          />
+              :label="$t('btn_register')"
+              color="primary"
+              class="full-width"
+              size="md"
+              flat to="/register"
+            />
+        </div>
+
+        <div class=" row justify-center">
           <q-btn
             :label="$t('btn_forgot_password')"
             color="primary"
             class="full-width"
-            size="sm"
+            size="md"
             flat
             :to="{ name: 'forgot-password' }"
           />
@@ -61,6 +68,7 @@ export default defineComponent({
     const router = useRouter()
     const { isLoggedIn, login } = useAuthUser()
     const { notifyError, notifySuccess } = useNotify()
+    const isPwd = ref(true)
 
     const form = ref({
       email: '',
@@ -84,6 +92,7 @@ export default defineComponent({
     }
     return {
       form,
+      isPwd,
       handleLogin
     }
   }
